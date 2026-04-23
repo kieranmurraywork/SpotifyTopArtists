c854f3084be57c0324e1bb7f081437a7c7fc9acd
Kieran Murray <kieranmurraywork@gmail.com>
2026-04-23 16:16:53 +0100

    23/04 - incorporated slideshow into main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import contextlib
 import os
 from io import BytesIO
 from urllib import request
 
 import cv2
+import imageio
 import numpy as np
 from spotipy import Spotify
 from matplotlib import pyplot as plt
@@ -10,6 +12,7 @@ from dotenv import load_dotenv
 from PIL import Image
 from spotipy.oauth2 import SpotifyOAuth
 import base64
+import glob
 
 
 def create_spotify_client() -> Spotify:
@@ -50,6 +53,31 @@ def create_playlist(sp, results,playlistCover):
     playlistB64 = base64.b64encode(playlistBytes)
     sp.playlist_upload_cover_image(playlist["id"], image_b64=playlistB64)
 
+def create_slideshow(imageList):
+    with imageio.get_writer('movie.mp4', mode='I') as writer:  # set the format to mp4 importing multiple images
+        for i in range(len(imageList)):  # Loop for appending all images in imageList to the outputted mp4
+            image = imageList[i]  # set image to the image in the ith element
+            writer.append_data(image)  # append this image to the writer
+""" #Commented out as code is currently not working in this iteration
+
+    ###### The below code return a gif but requires downloaded images #############
+    # filepaths
+    fp_in = "Top Songs/Top Songs *.png"
+    fp_out = "image.gif"
+
+    # use exit stack to automatically close opened images
+    with contextlib.ExitStack() as stack:
+        # lazily load images
+        imgs = (stack.enter_context(Image.open(f))
+                for f in sorted(glob.glob(fp_in)))
+
+        # extract  first image from iterator
+        img = next(imgs)
+
+        # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+        img.save(fp=fp_out, format='GIF', append_images=imgs,
+                 save_all=True, duration=200, loop=0)
+"""
 def main():
     from argparse import ArgumentParser
 
@@ -77,12 +105,21 @@ def main():
         type = bool,
         default = False,
         help = "Generate a playlist for the top 50 songs (default: False)")
+    parser.add_argument(
+        "-s",
+        "--slideshow",
+        metavar="slideshow",
+        type = bool,
+        default = False,
+        help = "Generate a slideshow for the top 50 songs (default: False)"
+        )
 
     args = parser.parse_args()
 
     selected_range: str = args.range
     selected_limit: int = args.limit
     selected_playlist: bool = args.playlist
+    selected_slideshow: bool = args.slideshow
 
     sp = create_spotify_client()
 
@@ -140,10 +177,9 @@ def main():
         j
     ]  # initialises averages to be the numpy array of the first photo
     while j < i:  # While the counter is less than the length of the number of images:
-        readInFile = imageList[j + 1]  # read in the value of the next photo in the list
-        averages = averages.astype(int) + readInFile.astype(
-            int
-        )  # combine the values of both numpy arrays
+        readInFile = imageList[j+1]  # read in the value of the next photo in the list
+        readInFileFixed = np.resize(readInFile,(640 ,640 ,3))
+        averages = averages.astype(int) + readInFileFixed.astype(int)  # combine the values of both numpy arrays
         j += 1  # increment j
     averages = averages / len(
         imageList
@@ -156,6 +192,8 @@ def main():
     outputImage.show()  # display the image
     if selected_playlist==True:
         create_playlist(sp, results,outputImage)
+    if selected_slideshow==True:
+        create_slideshow(imageList)
 
 
 if __name__ == "__main__":
